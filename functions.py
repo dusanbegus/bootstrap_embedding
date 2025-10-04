ecd99f4108124d5af639f9d991338d8be28aea16
Dusan Begus <dusan_begus@brown.edu>
2025-10-04 13:46:15 -0400

    fixed the absolute value issue

--- functions.py
+++ functions.py
@@ -57,7 +57,7 @@ def loss(c, angles, target_state):
     result = job.result()
     counts = result.get_counts(qc)
     num_zeros = counts.get('0', 0)
-    fidelity = np.sqrt(2*(num_zeros / 1000)-1.0)
+    fidelity = np.sqrt(np.absolute(2*(num_zeros / 1000)-1.0))
     
     return 1-fidelity
 
