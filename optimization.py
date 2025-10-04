9590b558b8a276412c277e37b1c7e5303d8811e0
Dusan Begus <dusan_begus@brown.edu>
2025-10-04 13:45:55 -0400

    refactored a bit

--- optimization.py
+++ optimization.py
@@ -27,6 +27,8 @@ def gradient_descent(c, angles, target, steps=1000, eta=0.01):
     original_loss=functions.loss(c, angles, target_state=target)
     minimal_loss=original_loss
     angles_minimal=angles
+    initial_innerproduct=np.inner(c.detach().numpy()*np.exp(1j*angles.detach().numpy()),target)
+    initial_fidelity=1-original_loss
     for step in range(steps):
         angles_grad = optimizer.gradient(c,angles, target, eta)
         angles_f = optimizer.optimizer_step(angles, eta, angles_grad)
@@ -38,24 +40,24 @@ def gradient_descent(c, angles, target, steps=1000, eta=0.01):
                 minimal_loss=loss_value
                 angles_minimal=angles_f
             if loss_value<previous:
-                eta=eta*0.9
+                eta=eta*0.7
                 previous=loss_value
             else:
                 eta=eta*1.2
                 previous=loss_value
-            print(f"Step {step}, Loss: {loss_value}, Learning Rate: {eta}")
-    return c, angles_minimal, minimal_loss
+    return c, angles_minimal, minimal_loss, initial_innerproduct, initial_fidelity
 if __name__ == '__main__':
-    target=functions.create_random()*np.exp(1j*np.random.rand(32))
+    target=functions.create_random()*np.exp(1j*np.random.rand(32)*2*np.pi)
     a=gradient_descent(torch.tensor(setup.initialize_c(target,setup.basis(5))), torch.tensor(functions.create_init()),
                                         torch.tensor(target),
-                                        steps=50)
+                                        steps=100, eta=0.1)
     # we now compute the fidelity by subtracting the loss from 1
     fidelity = 1 - a[2] 
-    print("target state:  ", target)
-    print("optimized state:  ", a[0]*np.exp(1j*a[1].detach().numpy()))
-    # we want to compute the inner product between the two states
-    print("inner product", np.inner(a[0]*np.exp(1j*a[1].detach().numpy()),target))
+    print("target state: ", target)
+    print("initial norm of inner product with target state", np.linalg.norm(a[3]))
+    print("initial fidelity with target state: ", a[4])
+    # we want to compute the inner product between the two states after the optimization is performed
+    print("norm of inner product with target state in the end", np.linalg.norm(np.inner(a[0]*np.exp(1j*a[1].detach().numpy()),target)))
     print(f"Fidelity with target state: {fidelity}")    
     
     sys.exit(0)
